6607556300285c61aa5b736506580b043f97fead
xRonnyn <pashuha.k@gmail.com>
2025-12-05 12:38:50 +0300

    p_t + f_t

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 
-struct p_t {
-  int x, y;
-};
-struct IDraw {
-  virtual p_t begin() const = 0;
-  virtual p_t next(p_t) const = 0;
-  virtual nIDraw();
-};
+namespace topit {
+  struct p_t {
+    int x, y;
+  };
+  struct f_t {
+    p_t aa, bb;
+  }; // pod
+  bool operator==(p_t, p_t);
+  bool operator!=(p_t, p_t);
+} // namespace topit
 
-int main() {}
+int main() {
+  using topit::p_t;
+  p_t a{1,0}, b{1,0};
+  std::cout << (a == b) << "\n";
+}
+bool topit::operator==(p_t a, p_t b) {
+  return a.x == b.x && a.y == b.y;
+}
+bool topit::operator!=(p_t a, p_t b) {
+  return !(a==b);
+}
